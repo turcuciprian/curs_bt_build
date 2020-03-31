5b24d64725a6da8e2c66372ea89f821769ba76e4
Ciprian Turcu <turcuciprian1@gmail.com>
2020-03-31 23:51:01 +0300

    updated

---
+++ precache-manifest.c245cf437ffa088473c10d92ee4693c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c20f392714b4a80753546635838f0c10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e80225590ee44e176deb",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "5bf8bd34a45295490e59",
+    "url": "/static/js/2.957b884a.chunk.js"
+  },
+  {
+    "revision": "2d1f0ed21c5c95e3a2cb212dbd2653e2",
+    "url": "/static/js/2.957b884a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e80225590ee44e176deb",
+    "url": "/static/js/main.50d4ed4f.chunk.js"
+  },
+  {
+    "revision": "1310a5804204306b74cc",
+    "url": "/static/js/runtime-main.ff2b558b.js"
+  }
+]);
