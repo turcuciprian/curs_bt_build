2184fd0aa976870f031a71a0d2e6a1f8e6631774
Ciprian Turcu <turcuciprian1@gmail.com>
2020-04-08 21:23:09 +0300

    added noselect for mobile when dragging

---
+++ precache-manifest.257ec838e269aed638d5a5f46974cca2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40348e0c5c094d1d226a75e9d22d6a31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "688288a540e2d49c3db2",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "171c7b0b20a39b55f8d3",
+    "url": "/static/js/2.85d0c276.chunk.js"
+  },
+  {
+    "revision": "2d1f0ed21c5c95e3a2cb212dbd2653e2",
+    "url": "/static/js/2.85d0c276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "688288a540e2d49c3db2",
+    "url": "/static/js/main.6ba95573.chunk.js"
+  },
+  {
+    "revision": "1310a5804204306b74cc",
+    "url": "/static/js/runtime-main.ff2b558b.js"
+  }
+]);
