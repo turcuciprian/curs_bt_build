115994feb562969534411504581c700e20ea3d17
Ciprian Turcu <turcuciprian1@gmail.com>
2020-04-08 14:16:36 +0300

    added drag and drop functionality

---
+++ precache-manifest.0bbc682b9b461b8552fe2ce1608c50bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0b440238580fa47ad504b05f22ad3e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "597792cb48887d51ae95",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "171c7b0b20a39b55f8d3",
+    "url": "/static/js/2.85d0c276.chunk.js"
+  },
+  {
+    "revision": "2d1f0ed21c5c95e3a2cb212dbd2653e2",
+    "url": "/static/js/2.85d0c276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "597792cb48887d51ae95",
+    "url": "/static/js/main.b4766753.chunk.js"
+  },
+  {
+    "revision": "1310a5804204306b74cc",
+    "url": "/static/js/runtime-main.ff2b558b.js"
+  }
+]);
